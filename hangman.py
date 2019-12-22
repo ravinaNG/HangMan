86e19cd0e17192d4ee5c948f48e652cf748e1016
ravinaNG <ravina18@navgurukul.org>
2019-12-22 12:04:24 +0530

    Added dificulty level for user

--- hangman.py
+++ hangman.py
@@ -66,8 +66,20 @@ def hangman(secret_word):
   print ("I am thinking of a word that is " + str(len(secret_word)) + " letters long.")
   print ("")
 
+  user_difficulty_choice = input('Difficulty level- \na)\tEasy\nb)\tMedium\nc)\tHard\n\nYour choice should be in a, b and c terms. ')
+  total_lives = remaining_lives = 8
+  images_selection_list_indices = [0, 1, 2, 3, 4, 5, 6, 7]
+  if user_difficulty_choice not in ['a', 'b', 'c']:
+      print ('Invalid choice. \nGame is starting in Easy mode.')
+  else:
+      if user_difficulty_choice == 'b':
+          total_lives = remaining_lives = 6
+          images_selection_list_indices = [0, 2, 3, 5, 6, 7]
+      elif user_difficulty_choice == 'c':
+          total_lives = remaining_lives = 4
+          images_selection_list_indices = [1, 3, 5, 7]
+
   letters_guessed = []
-  remaining_lives = 8
   while (remaining_lives > 0):
     
     available_letters = get_available_letters(letters_guessed)
@@ -94,10 +106,12 @@ def hangman(secret_word):
             break
     else:
         print ("Oops! That letter is not in my word: " + get_guessed_word(secret_word, letters_guessed))
-        print (IMAGES[8-remaining_lives])
+        image = images_selection_list_indices[total_lives-remaining_lives]
+        print (IMAGES[image])
         letters_guessed.append(letter)
         print ("")
-        remaining_lives = remaining_lives - 1
+        remaining_lives -= 1
+        print ('Remaining Live : ', remaining_lives)
   
 # Load the list of words into the variable wordlist
 # So that it can be accessed from anywhere in the program
